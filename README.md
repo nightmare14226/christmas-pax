40e2fc590cdd15624e5c1a2518743af25c35a177
nightmare14226 <miltonjohnston14226>
2023-12-23 10:00:34 +0900

    feat: first release

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paxintrade
+
+## Merry Christmas!!!
---
+++ src/Experience/sources.js
@@ -0,0 +1,48 @@
+export default [
+    // {
+    //     name: 'backgroundSound',
+    //     type: 'audio',
+    //     path: 'sounds/background.mp3'
+    // },
+    // {
+    //     name: 'predatorModel',
+    //     type: 'gltfModel',
+    //     path: 'models/predator.glb'
+    // },
+    // {
+    //     name: 'displacementTexture',
+    //     type: 'texture',
+    //     path: 'textures/displacement.jpg'
+    // }
+
+    {
+        name: 'e2Model',
+        type: 'gltfModel',
+        path: 'models/Pax_2024.glb'
+    },
+    // {
+    //     name: 'dnaModel',
+    //     type: 'gltfModel',
+    //     path: 'models/dna.glb'
+    // },
+    {
+        name: 'boyModel',
+        type: 'gltfModel',
+        path: 'models/Pax_letter.glb'
+    },
+    {
+        name: 'oniModel',
+        type: 'gltfModel',
+        path: 'models/Pax.glb'
+    },
+    {
+        name: 'horseModel',
+        type: 'gltfModel',
+        path: 'models/horse.glb'
+    },
+    {
+        name: 'treeModel',
+        type: 'gltfModel',
+        path: 'models/tree.glb'
+    },
+]
